f6ce71c3dcc7f8a11a3d3a1b16bc3268fb3bd284
Kato Damian <katodakam2010@gmail.com>
2022-02-19 17:14:53 -0700

    UI design and Main Activity functionality added.

--- app/src/main/java/com/dakam/dinnerdeciderapp/MainActivity.kt
+++ app/src/main/java/com/dakam/dinnerdeciderapp/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.dakam.dinnerdeciderapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var foodList:ArrayList<String> = arrayListOf("Hamburger", "Pizza","Mexican", "American", "Chinese")
+        var btnDecide = findViewById<Button>(R.id.btn_decide);
+        var btnAddFood = findViewById<Button>(R.id.btn_addfood);
+        var txtFoodType = findViewById<TextView>(R.id.txt_foodType);
+        var txtEditFodName = findViewById<EditText>(R.id.txt_addfood);
+
+        btnDecide.setOnClickListener { v ->
+            var len:Int = foodList.size
+            var randIndex:Int = (0 until len-1).random()
+            txtFoodType.text=  foodList.get(randIndex)
+        }
+
+        btnAddFood.setOnClickListener { v->
+            var foodName:String
+            if(txtEditFodName.text.toString()!=null) {
+                foodName = txtEditFodName.text.toString()
+                foodList.add(foodName)
+                txtEditFodName.setText("")
+
+            }
+        }
     }
 }
